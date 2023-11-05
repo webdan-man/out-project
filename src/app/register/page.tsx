33355cc48f369aac078f6d0911d7f0005fc867ef
k.yanchitskyi <k.yanchitskyi@softblues.io>
2023-11-05 21:07:16 +0200

    second commit

--- src/app/register/page.tsx
+++
@@ -1,9 +0,0 @@
-import RegistrationForm from '@/components/RegistrationForm'
-
-export default function RegisterPage() {
-    return (
-        <main className="px-4 m-0">
-            <RegistrationForm />
-        </main>
-    );
-}
